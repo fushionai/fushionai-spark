5783442aaa06efe36988fa18f4597c3d565b6639
fushionai <fushionai.business@gmail.com>
2025-09-30 22:23:31 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/fusionai-spark/',   // 👈 toegevoegd
   server: {
     host: "::",
     port: 8080,
